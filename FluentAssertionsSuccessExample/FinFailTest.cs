595fcb50b15f71c0e5c3cfbf2dedd562c5282523
Ivan Kashtanov <iakashtanov@gmail.com>
2023-11-05 13:21:17 +0400

    Add minimal example for lib error

---
+++ FluentAssertionsSuccessExample/FinFailTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using FluentAssertions.LanguageExt;
+using LanguageExt.Common;
+
+namespace FluentAssertionsSuccessExample;
+
+public class FinFailTest
+{
+    [Fact]
+    public void BeFail_Should_Not_Throw_Missing_Method_Exception()
+    {
+        var action = () => FinFail<int>(Error.New("")).Should().BeFail();
+        action.Should().NotThrow();
+    }
+}
